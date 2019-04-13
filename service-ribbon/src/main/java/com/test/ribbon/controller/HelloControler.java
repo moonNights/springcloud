39fb04e503d6b5ea49a0699ad8fa7e1030974c04
moonNights <strSpecial@163.com>
2019-04-13 10:00:35 +0800

    [init repository file]

---
+++ service-ribbon/src/main/java/com/test/ribbon/controller/HelloControler.java
@@ -0,0 +1,18 @@
+package com.test.ribbon.controller;
+
+import com.test.ribbon.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloControler {
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping("/hi")
+    public String hi(@RequestParam String name) {
+        return helloService.hiService(name);
+    }
+}
